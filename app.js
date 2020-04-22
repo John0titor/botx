1dd54553b730d3092e2ad6a6c574a7b0c86029ee
John0titor <32824831+John0titor@users.noreply.github.com>
2020-04-22 18:06:46 -0300

    changes

---
+++ app.js
@@ -0,0 +1,23 @@
+var Twitter = require('twitter');
+var arr = require('./arr.js');
+const randomItem = require('random-item');
+require('dotenv/config');
+
+var T = new Twitter({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token_key: process.env.access_token_key,
+    access_token_secret: process.env.access_token_secret
+});
+
+var selectArr = randomItem (arr);
+
+function myFunction() {
+    T.post('statuses/update', {status: selectArr}, function(error, tweet, response) {
+        if (error) console.log("error", error);
+        else console.log("Tweet enviado." + selectArr);
+    });
+};
+
+myFunction();
+setInterval(myFunction, 28800000);
